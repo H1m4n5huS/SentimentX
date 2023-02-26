791677f254141dedd9a2e57e40a693628bbd36ca
anirudhkarumuri <anirudhkarumuri@gmail.com>
2023-02-26 07:14:41 +0100

    Added word cloud plotting function.

--- utils/Visualizer.py
+++ utils/Visualizer.py
@@ -1,5 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from IPython.display import display, Image
 
 
 class PostProcessor:
@@ -27,3 +29,18 @@ class PostProcessor:
         elif len(value["polarity"]) != 0:
             self._sentiments = value
 
+    def plot_word_cloud(self) -> None:
+        """
+        Function that displays a word cloud
+        :return: None
+        """
+        wordcloud = WordCloud(width=800, height=800,
+                              background_color='white',
+                              min_font_size=10).generate(self._sentiments["polarity"])
+        plt.figure(figsize=(8, 8), facecolor=None)
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.tight_layout(pad=0)
+        plt.show()
+        # Will work only in jupyter notebook.
+        display(Image(filename='twitter_wordcloud.png'))
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,9 @@
+# Authors: Kartik Kohli, Sai Anirudh Karumuri, Srishti, , Himanshu, Nidhi Mathur, Sukant
+# Contributors: Parag Raut, Praveen Vyas
+# BYOP Project IPBA 13
+
 from .ScrapeData import *
 from .pre_processing import *
+from .Visualizer import *
+
+
