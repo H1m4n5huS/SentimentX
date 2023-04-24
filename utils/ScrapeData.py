9084e55daf8c2f0e8b32fc0f2ca208ab8b7ad4e5
anirudhkarumuri <anirudhkarumuri@gmail.com>
2023-04-24 23:30:37 +0200

    commented youtube extract

--- utils/ScrapeData.py
+++ utils/ScrapeData.py
@@ -2,8 +2,8 @@ import csv
 import os
 import datetime
 import snscrape.modules.twitter as sntwitter
-import googleapiclient
-from googleapiclient.discovery import build
+# import googleapiclient
+# from googleapiclient.discovery import build
 
 
 # start_date = datetime.datetime(2019, 1, 13)
@@ -88,7 +88,7 @@ class YoutubeExtract:
         datasets_dir = os.path.join(os.getcwd(), "datasets")
         filename = os.path.join(datasets_dir, self._hashtag[1:])
         # creating youtube resource object
-        youtube = build('youtube', 'v3', developerKey=self.api_key)
+        # youtube = build('youtube', 'v3', developerKey=self.api_key)
 
         # create a CSV file for writing the data to
         with open(f'comments_for_video_{self._video_id}.csv', 'w', newline='', encoding='utf-8') as file:
@@ -103,63 +103,63 @@ class YoutubeExtract:
             reply_counter = 1
 
             # retrieve the video comments
-            video_response = youtube.commentThreads().list(
-                part='snippet',
-                videoId=self._video_id,
-                textFormat='plainText'
-            ).execute()
+            # video_response = youtube.commentThreads().list(
+            #     part='snippet',
+            #     videoId=self._video_id,
+            #     textFormat='plainText'
+            # ).execute()
 
             # iterate through the video comments
-            while video_response:
-                # iterate through each item in the video comments
-                for item in video_response['items']:
-                    # extract the comment data
-                    comment = item['snippet']['topLevelComment']['snippet']
-                    comment_text = comment['textDisplay']
-                    comment_published_at = comment['publishedAt']
-                    comment_like_count = comment['likeCount']
-                    # write the comment data to the CSV file
-                    writer.writerow({'Comment Number': comment_counter,
-                                     'Reply Number': '',
-                                     'Like Count': comment_like_count,
-                                     'Published At': comment_published_at,
-                                     'Text': comment_text})
-                    # increment the comment counter
-                    comment_counter += 1
-
-                    # retrieve the replies to the comment
-                    if item['snippet']['totalReplyCount'] > 0:
-                        reply_response = youtube.comments().list(
-                            part='snippet',
-                            parentId=item['snippet']['topLevelComment']['id'],
-                            textFormat='plainText'
-                        ).execute()
-
-                        # iterate through the replies to the comment
-                        for reply in reply_response['items']:
-                            reply_text = reply['snippet']['textDisplay']
-                            reply_published_at = reply['snippet']['publishedAt']
-                            if 'likeCount' in reply:
-                                reply_like_count = reply['likeCount']
-                            else:
-                                reply_like_count = 0
-                            # write the reply data to the CSV file
-                            writer.writerow({'Comment Number': '',
-                                             'Reply Number': reply_counter,
-                                             'Like Count': reply_like_count,
-                                             'Published At': reply_published_at,
-                                             'Text': reply_text})
-                            # increment the reply counter
-                            reply_counter += 1
-
-                # check if there are more comments to retrieve
-                if 'nextPageToken' in video_response:
-                    video_response = youtube.commentThreads().list(
-                        part='snippet',
-                        videoId=self._video_id,
-                        textFormat='plainText',
-                        pageToken=video_response['nextPageToken']
-                    ).execute()
-                else:
-                    break
-        print(f"CSV file saved at {filename}")
+        #     # while video_response:
+        #         # iterate through each item in the video comments
+        #         for item in video_response['items']:
+        #             # extract the comment data
+        #             comment = item['snippet']['topLevelComment']['snippet']
+        #             comment_text = comment['textDisplay']
+        #             comment_published_at = comment['publishedAt']
+        #             comment_like_count = comment['likeCount']
+        #             # write the comment data to the CSV file
+        #             writer.writerow({'Comment Number': comment_counter,
+        #                              'Reply Number': '',
+        #                              'Like Count': comment_like_count,
+        #                              'Published At': comment_published_at,
+        #                              'Text': comment_text})
+        #             # increment the comment counter
+        #             comment_counter += 1
+        #
+        #             # retrieve the replies to the comment
+        #             if item['snippet']['totalReplyCount'] > 0:
+        #                 reply_response = youtube.comments().list(
+        #                     part='snippet',
+        #                     parentId=item['snippet']['topLevelComment']['id'],
+        #                     textFormat='plainText'
+        #                 ).execute()
+        #
+        #                 # iterate through the replies to the comment
+        #                 for reply in reply_response['items']:
+        #                     reply_text = reply['snippet']['textDisplay']
+        #                     reply_published_at = reply['snippet']['publishedAt']
+        #                     if 'likeCount' in reply:
+        #                         reply_like_count = reply['likeCount']
+        #                     else:
+        #                         reply_like_count = 0
+        #                     # write the reply data to the CSV file
+        #                     writer.writerow({'Comment Number': '',
+        #                                      'Reply Number': reply_counter,
+        #                                      'Like Count': reply_like_count,
+        #                                      'Published At': reply_published_at,
+        #                                      'Text': reply_text})
+        #                     # increment the reply counter
+        #                     reply_counter += 1
+        #
+        #         # check if there are more comments to retrieve
+        #         if 'nextPageToken' in video_response:
+        #             video_response = youtube.commentThreads().list(
+        #                 part='snippet',
+        #                 videoId=self._video_id,
+        #                 textFormat='plainText',
+        #                 pageToken=video_response['nextPageToken']
+        #             ).execute()
+        #         else:
+        #             break
+        # print(f"CSV file saved at {filename}")
